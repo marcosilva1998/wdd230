d671710734c2a3697f859a0142a7e0c59f8976a4
Marco Silva <marksilvamoreno@hotmail.com>
2024-05-23 22:56:46 -0300

    Proove 5

--- scripts/links.js
+++ scripts/links.js
@@ -3,7 +3,7 @@ const linksURL = "https://marcosilva1998.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
   }
   
   getLinks();
