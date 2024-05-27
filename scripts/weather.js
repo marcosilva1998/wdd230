57558ec9672f291e608e3e3e04908c30fc9fea52
Marco Silva <marksilvamoreno@hotmail.com>
2024-05-27 19:07:05 -0300

    activity

---
+++ scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+const url ='https://api.openweathermap.org/data/2.5/weather?lat=-34.9&lon=-57.95&units=imperial&appid=c192a3206cec2928b6002659ccdaecfc'
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data);
+            displayWeather(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+apiFetch() ;
+const displayWeather = (data) => {
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    currentTemp.innerHTML = `${data.main.temp}&deg;F - ${desc}`;
+ 
+}
