d26d7cf477e7f5c7eb19a039409819621980681f
Marco Silva <marksilvamoreno@hotmail.com>
2024-06-02 18:38:10 -0300

    Fixed log timestamp

--- chamber/scripts/time_stamp.js
+++ chamber/scripts/time_stamp.js
@@ -2,6 +2,5 @@ function time() {
     const timestampField = document.getElementById('timestamp');
     let currentTime = new Date().getTime();
     timestampField.value = currentTime;
-    console.log(currentTime);
 }
 time()
